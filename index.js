77b3aa39605ae4100d21fdb2ad3e609bd1bbf34b
usama <usama999abbasi@gmail.com>
2020-05-19 00:00:29 +0500

    Assignment no 1

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http=require('http');
 const morgan=require('morgan');
 const bodyParser=require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const leaderRouter=require('./routes/leaderRouter');
+const promoRouter=require('./routes/promoRouter');
 const hostname='localhost';
 const port=3000;
 const app=express();
@@ -10,6 +12,8 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json());
 app.use('/dishes',dishRouter);
+app.use('/leaders',leaderRouter);
+app.use('/promotions',promoRouter);
 app.use((req,res,next)=>{
     console.log(req.headers);
     res.statusCode=200;
