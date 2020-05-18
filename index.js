aa095abfa84c5dcacd7fd6a65bbffca258b60a89
usama <usama999abbasi@gmail.com>
2020-05-18 20:20:45 +0500

    Express Serve Static Files

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express=require('express');
 const http=require('http');
+const morgan=require('morgan');
 const hostname='localhost';
 const port=3000;
 const app=express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname+'/public'));
 app.use((req,res,next)=>{
     console.log(req.headers);
     res.statusCode=200;
